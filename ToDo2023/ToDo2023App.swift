627d5b4ab1ed1deb92f4bc9e5afc896805d28eb3
mersakakey <75288670+mersakakey@users.noreply.github.com>
2023-05-08 16:56:53 +0900

    Initial Commit

---
+++ ToDo2023/ToDo2023App.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo2023App.swift
+//  ToDo2023
+//
+//  Created by Takeru Sakakibara on 2023/05/08.
+//
+
+import SwiftUI
+
+@main
+struct ToDo2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
